8dd783fb26e3b0737e8e3b50d880af7891e232c2
Kevin Bottoms <webdev@kevinbottoms.com>
2016-03-12 20:31:05 +0100

    Added Duplicate Item Error Moved method to add to @@products to private
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
    Changes to be committed:
    modified:   app.rb
    modified:   lib/errors.rb
    modified:   lib/product.rb

--- app.rb
+++ app.rb
@@ -11,8 +11,9 @@ Product.new(title: "LEGO Firehouse Headquarter", price: 199.99, stock: 0)
 
 puts Product.all.count # Should return 3
 
-# Product.new(title: "LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
-# Should return DuplicateProductError: 'LEGO Iron Man vs. Ultron' already exists.
+#Product.new(title: "LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
+#Should return DuplicateProductError: 'LEGO Iron Man vs. Ultron' already exists.
+puts Product.all.inspect
 
 # nanoblock = Product.find_by_title("Nano Block Empire State Building")
 # firehouse = Product.find_by_title("LEGO Firehouse Headquarter")
--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,25 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    Product.all.each do |item|
+      if item.title == "#{@title}"
+        raise DuplicateProductError, "'#{:title}' already exists."
+      end
+    end
+    @@products << self
   end
 end
