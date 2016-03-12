bb8f2c850f6de74e0d3996f1326377c651a5143d
Kevin Bottoms <webdev@kevinbottoms.com>
2016-03-12 21:57:48 +0100

    Added:   -Class Methods     -find_by_title     -in_stock   -Instance Methods     -in_stock?   -Attributes     -price     -stock
    Your branch is up-to-date with 'origin/master'.
    
    Changes to be committed:
    modified:   app.rb
    modified:   lib/product.rb

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,43 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
+  end
+
+  def in_stock?
+    if @stock > 0
+      return true
+    else
+      return false
+    end
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title (name)
+    Product.all.each do |item|
+      if item.title == name
+        return item
+      end
+    end
+  end
+
+  def self.in_stock
+    stocked = []
+    Product.all.each do |item|
+      if item.stock > 0
+        stocked << item
+      end
+    end
+    return stocked
   end
 
   private
